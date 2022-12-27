358abe9c611275d1bfb43046a8388d6d1e1c2b9a
Franco Ojeda <fojedavideos@gmail.com>
2022-12-27 09:55:09 -0500

    implementacion card y funciones

--- src/app/componentes/cards/cards.component.ts
+++ src/app/componentes/cards/cards.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardsComponent implements OnInit {
 
+  num:number = 0;
+
+  aumentar(){
+    this.num++;
+  }
+  disminuir(){
+    this.num--;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
